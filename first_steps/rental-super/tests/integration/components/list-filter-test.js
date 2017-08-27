8309437cd905d94d8995487e9b2411ad0cfcd0e4
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-27 10:29:25 -0300

    criando um app com nested routes, tests, google maps api and searching

---
+++ first_steps/rental-super/tests/integration/components/list-filter-test.js
@@ -0,0 +1,47 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import RSVP from 'rsvp';
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
+
+moduleForComponent('list-filter', 'Integration | Component | filter listing', {
+  integration: true
+});
+
+test('should initially load all listings', function (assert) {
+  this.on('filterByCity', () => {
+    return RSVP.resolve({ results: ITEMS });
+  });
+
+  this.render(hbs`
+    {{#list-filter filter=(action 'filterByCity') as |results|}}
+      <ul>
+      {{#each results as |item|}}
+        <li class="city">
+          {{item.city}}
+        </li>
+      {{/each}}
+      </ul>
+    {{/list-filter}}
+  `);
+
+  // The keyup event here should invoke an action that will cause the list to be filtered
+  this.$('.list-filter input').val('San').keyup();
+
+  return wait().then(() => {
+    assert.equal(this.$('.city').length, 3);
+    assert.equal(this.$('.city').first().text().trim(), 'San Francisco');
+  });
+});
+
+test('should filter the list of rentals by city.', function (assert) {
+  visit('/');
+  fillIn('.list-filter input', 'Seattle');
+  keyEvent('.list-filter input', 'keyup', 69);
+  andThen(function() {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+  });
+});

408986c28055420913330f6ddf75225ad0f16b02
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-28 18:33:52 -0300

    Lesson 5 Finished

--- challenge_first_step/app-library/app/routes/libraries/edit.js
+++ challenge_first_step/app-library/app/routes/libraries/edit.js
@@ -5,6 +5,18 @@ export default Ember.Route.extend({
     return this.store.findRecord('library', params.library_id);
   },
 
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Edit library');
+    controller.set('buttonLabel', 'Save changes');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
+
   actions: {
 
     saveLibrary(library) {
@@ -12,7 +24,6 @@ export default Ember.Route.extend({
     },
 
     willTransition(transition) {
-
       let model = this.controller.get('model');
 
       if (model.get('hasDirtyAttributes')) {
